43c7962acdad09e626c6622def53e1cb9317d569
RaghadAlshalaan <55341928+RaghadAlshalaan@users.noreply.github.com>
2019-11-25 08:36:47 +0300

    Add files via upload

---
+++ OS/src/OS/os.java
@@ -0,0 +1,25 @@
+package OS;
+public class os {
+	
+	int aAccount =3000 ;
+	int bAccount =4000 ;
+	
+	
+	public static void main (String []  args) {
+		
+		
+		
+		
+		
+	}// end main
+	
+	private void initThreads() {
+		Thread t1 = new Thread();
+		Thread t2 = new Thread();
+
+		
+		
+		
+	}//initThreads
+
+}// end class
---
+++ OS/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author raghadaziz
+ *
+ */
+module OS {
+}
